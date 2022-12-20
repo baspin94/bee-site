69f419eff5918f6a92ed6a3489e7a1f73d8a4a96
Bianca Aspin <bianca@mj94.net>
2022-12-20 12:46:23 -0800

    Added dog 'boop' script, updated HTML to include instructive caption underneath pictures.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript!";
-document.querySelector("body").appendChild(h2);
-
 const audio = new Audio("audio/bianca_pronunciation.mp3");
 const input = document.querySelector("button");
 
 input.addEventListener("click", function() {
     audio.play();
+});
+
+const daisy = document.getElementById("Daisy");
+const dolly = document.getElementById("Dolly");
+
+daisy.addEventListener("click", function(){
+    alert("Daisy: 'HAI! Thank you for the boop! Here, have some smoochies!'");
+});
+
+dolly.addEventListener("click", function() {
+    alert("Dolly: 'Ugh, go away. I'm not interested in boops, only food.'");
 });
