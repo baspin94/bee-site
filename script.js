f54e7fc1a47ae9dc9f505eeb76d69158bc59c4e2
Bianca Aspin <bianca@mj94.net>
2022-12-19 19:30:55 -0800

    Added script for audio playback on button-click, HTML formatting clean-up, photo swap

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript!";
 document.querySelector("body").appendChild(h2);
+
+const audio = new Audio("audio/bianca_pronunciation.mp3");
+const input = document.querySelector("button");
+
+input.addEventListener("click", function() {
+    audio.play();
+});
